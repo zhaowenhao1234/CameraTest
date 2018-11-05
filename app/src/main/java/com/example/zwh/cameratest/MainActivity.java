8eda93889fcd1063da1afee1d706f09e36eed4ea
zhaowenhao1234 <975061263@qq.com>
2018-11-05 13:40:47 +0800

    从相册获得图片（解决4.4以上版本bug）

--- app/src/main/java/com/example/zwh/cameratest/MainActivity.java
+++ app/src/main/java/com/example/zwh/cameratest/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.zwh.cameratest;
 
 import android.Manifest;
+import android.content.ContentUris;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -17,6 +20,7 @@ import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,10 +29,13 @@ import android.widget.ImageView;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Date;
+import java.util.jar.Pack200;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int TAKE_PHOTO = 1;
+    public static final int CHOOSE_PHOTO=2;
     private ImageView picture;
     private Uri imageUri;
 
@@ -37,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button takePhoto = findViewById(R.id.takephoto);
+        Button choosePhoto=findViewById(R.id.ablum);
         picture=findViewById(R.id.nihao);
         takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,6 +74,16 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        choosePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(MainActivity.this,Manifest.permission.WRITE_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
+                }else{
+                    openAblum();
+                }
+            }
+        });
     }
 
     @Override
@@ -87,6 +105,66 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
                 break;
+            case CHOOSE_PHOTO:
+                    if(resultCode==RESULT_OK){
+                        if(Build.VERSION.SDK_INT>=19){
+                            handleImageOnKitkat(data);
+                        }else{
+                            handleiamgebeforekitkat(data);
+                        }
+                    }
+                break;
+        }
+    }
+    public  void openAblum(){
+        Intent intent=new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("image/*");
+        startActivityForResult(intent,CHOOSE_PHOTO);
+    }
+    public void handleImageOnKitkat(Intent data){
+        String imagePath=null;
+        Uri uri=data.getData();
+        if(DocumentsContract.isDocumentUri(this,uri)){
+            String docId=DocumentsContract.getDocumentId(uri);
+            if("com.android.providers.media.documents".equals(uri.getAuthority())){
+                String id=docId.split(":")[1];
+                String selection=MediaStore.Images.Media._ID+"="+id;
+                imagePath=getimagepath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            }else if("com.android.providers.downloads.documents".equals(uri.getAuthority())){
+                Uri contenturi=ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),Long.valueOf(docId));
+                imagePath=getimagepath(contenturi,null);
+            }
+            else if ("content".equalsIgnoreCase(uri.getScheme())){
+                Log.d("nihao", uri.toString());
+               imagePath=getimagepath(uri,null);
+
+            }else if("file".equalsIgnoreCase(uri.getScheme())){
+                imagePath=uri.getPath();
+            }
+            display(imagePath);
+        }
+    }
+    public void handleiamgebeforekitkat(Intent data){
+        Uri uri=data.getData();
+        String imagepath=getimagepath(uri,null);
+        display(imagepath);
+    }
+    private String getimagepath(Uri uri,String selection){
+        String path=null;
+        Cursor cursor=getContentResolver().query(uri,null,selection,null,null);
+        if(cursor!=null){
+            {
+                int index=cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToFirst();
+                path=cursor.getString(index);
+            }cursor.close();
+        }
+        return  path;
+    }
+    private void display(String imagePath){
+        if(imagePath!=null){
+            Bitmap bitmap=BitmapFactory.decodeFile(imagePath);
+            picture.setImageBitmap(bitmap);
         }
     }
 }
